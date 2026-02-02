3f2c4e235ed39438a691e22f4385ab552eed0e43
wu-meng <wumeng1993@outlook.com>
2026-02-02 17:59:21 +0800

    增加资源

--- android/src/main/java/com/ncc/wm/android/AndroidLauncher.java
+++ android/src/main/java/com/ncc/wm/android/AndroidLauncher.java
@@ -6,13 +6,18 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.ncc.wm.Main;
 
-/** Launches the Android application. */
+/**
+ * Launches the Android application.
+ * AndroidApplication，这是 LibGDX 提供的一个封装了 Android 底层细节的基类，用于在 Android 上运行 LibGDX 应用
+ * */
 public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 用于配置 LibGDX 在 Android 平台上的运行参数
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-        configuration.useImmersiveMode = true;  // Recommended, but not required.
+        configuration.useImmersiveMode = true;  // 启用 沉浸式模式（Immersive Mode）,这会隐藏状态栏和导航栏，让游戏全屏显示，提供更好的沉浸体验
         initialize(new Main(), configuration);  // 将Main作为游戏入口传入
     }
 }
--- core/src/main/java/com/ncc/wm/Main.java
+++ core/src/main/java/com/ncc/wm/Main.java
@@ -6,22 +6,37 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
+ * */
 public class Main extends ApplicationAdapter {
 
+    // Texture: 纹理，代表图片素材
     Texture backgroundTexture;
     Texture bucketTexture;
     Texture dropTexture;
+
+    // Sound: 短小，频繁播放的音效
     Sound dropSound;
+
+    // Music: 长时间播放的音频
     Music music;
 
+    // SpriteBatch: 一个专门用来批量画图的画笔
     SpriteBatch spriteBatch;
+
+    // 窗口
     FitViewport viewport;
+
+    // 方便类，封装了 Texture + 位置 + 尺寸 + 旋转 + 颜色等属性
+    private Sprite bucketSprite;
 
     @Override
     public void create() {
@@ -34,11 +49,14 @@ public class Main extends ApplicationAdapter {
 
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(10,5);
+
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1,1);
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height, true);
+        viewport.update(width, height, true);   // true = 居中相机
     }
 
     /**
@@ -60,17 +78,27 @@ public class Main extends ApplicationAdapter {
     }
 
     private void draw() {
+        // Gdx.app.log("draw", String.valueOf(count));
+
         ScreenUtils.clear(Color.BLACK);
+
+        // 告诉 OpenGL：“接下来的渲染，请使用我定义的视口（Viewport）设置”
         viewport.apply();
+
+        // 将 SpriteBatch 的坐标系统 从默认的屏幕像素坐标，切换为你定义的 虚拟世界坐标系（比如 10×5）
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+
+        // 开始绘图
         spriteBatch.begin();
 
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
 
         spriteBatch.draw(backgroundTexture, 0,0, worldWidth, worldHeight);
-        spriteBatch.draw(bucketTexture, 0, 0, 1, 1);
+        // spriteBatch.draw(bucketTexture, 0, 0, 1, 1);
+        bucketSprite.draw(spriteBatch);
 
+        // 结束绘图，此时真正提交到GPU
         spriteBatch.end();
     }
 
